caf58281d595935abf6f7f690ce17b63f45356d7
rjohnson0707 <rwilljohnson@icloud.com>
2020-05-16 12:03:45 -0600

    Initialze Git

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Here will be my main App home page</p>
+        <p>Here will be my main App home page!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
